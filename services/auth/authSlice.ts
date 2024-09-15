dcf6537dbb5b4cf25db1a2271656b595042ff93f
sfgardev <sf.garol@gmail.com>
2024-09-15 13:49:27 +0700

    BD-17 fix:  change auth slice and add types

--- services/auth/authSlice.ts
+++ services/auth/authSlice.ts
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+import { SignInRequest, SignInResponse } from './types'
+
 // Define a service using a base URL and expected endpoints
 export const authApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
   endpoints: builder => ({
-    login: builder.mutation<any, void>({
-      query: () => 'auth/login',
+    login: builder.mutation<SignInResponse, SignInRequest>({
+      query: body => ({ body, method: 'POST', url: 'auth/login' }),
     }),
   }),
   reducerPath: 'auth',
