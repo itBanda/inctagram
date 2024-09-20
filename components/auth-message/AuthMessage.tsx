d4f498c3632257ac7cfd09222eb3e0fe0fe442fe
Kate <kate-ternovskaya99@mail.ru>
2024-09-20 11:57:57 +0300

    BD-19 fix: change button width

--- components/auth-message/AuthMessage.tsx
+++ components/auth-message/AuthMessage.tsx
@@ -14,7 +14,7 @@ export const AuthMessage = ({ buttonText, href, imageSrc, message, title }: Prop
     <div className='flex flex-col items-center py-9 text-center'>
       <Typography.H1 className='mb-5 text-light-100'>{title}</Typography.H1>
       <Typography.TextBase className='max-w-[294px] text-light-100'>{message}</Typography.TextBase>
-      <Button asChild className='mb-[72px] mt-14' variant='primary'>
+      <Button asChild className='mb-[72px] mt-14 min-w-44' variant='primary'>
         <Link href={href}>{buttonText}</Link>
       </Button>
       <Image alt={title} height={350} src={imageSrc} width={473} />
