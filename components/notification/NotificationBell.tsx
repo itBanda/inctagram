aeab0606b4063dfd249717011e60943f8cdab739
Kate <kate-ternovskaya99@mail.ru>
2024-11-07 12:23:16 +0300

    BD-73 feat: notifications styles

--- components/notification/NotificationBell.tsx
+++ components/notification/NotificationBell.tsx
@@ -2,31 +2,25 @@ import { useEffect, useState } from 'react'
 
 import { Icon, NotificationModal } from '@/components'
 import { exampleNotifications } from '@/components/notification/example-notifications'
+import { cn } from '@/utils'
 
 export const NotificationBell = () => {
   const [isOpen, setIsOpen] = useState(false)
-
-  const toggleNotifications = () => {
-    setIsOpen(!isOpen)
-  }
-
-  const handleEscape = (event: KeyboardEvent) => {
-    if (event.key === 'Escape') {
-      setIsOpen(false)
-    }
-  }
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      isOpen &&
-      event.target instanceof HTMLElement &&
-      !event.target.closest('.notification-modal')
-    ) {
-      setIsOpen(false)
-    }
-  }
+  const countNote = exampleNotifications?.length
+  const toggleNotifications = () => setIsOpen(!isOpen)
 
   useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => e.key === 'Escape' && setIsOpen(false)
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        event.target instanceof HTMLElement &&
+        !event.target.closest('.notification-modal')
+      ) {
+        setIsOpen(false)
+      }
+    }
+
     window.addEventListener('keydown', handleEscape)
     document.addEventListener('mousedown', handleClickOutside)
 
@@ -37,17 +31,22 @@ export const NotificationBell = () => {
   }, [isOpen])
 
   return (
-    <div className='relative hover:text-accent-100'>
+    <div className='relative'>
       <Icon
-        color='white'
+        className={isOpen ? 'text-accent-500' : 'transition hover:text-accent-500'}
         cursor='pointer'
-        icon='outline-bell'
+        icon={isOpen ? 'fill-bell' : 'outline-bell'}
         onClick={toggleNotifications}
-        width={36}
       />
-      {!!exampleNotifications.length && (
-        <div className='absolute -top-1 right-0 h-4 min-w-4 rounded-full bg-danger-500 px-1 text-center text-xs text-light-100'>
-          {exampleNotifications.length}
+      {!!countNote && (
+        <div
+          className={cn(
+            'absolute -right-1 -top-1 h-4 min-w-4 rounded-full bg-danger-500 px-1 text-center text-xs text-light-100',
+            { '-right-2.5': countNote > 19 },
+            { '-right-4': countNote > 99 }
+          )}
+        >
+          {countNote <= 99 ? countNote : '99+'}
         </div>
       )}
       {isOpen && (
--- components/notification/NotificationModal.tsx
+++ components/notification/NotificationModal.tsx
@@ -6,23 +6,23 @@ type Props = {
 
 export const NotificationModal = ({ notifications }: Props) => {
   return (
-    <div className='absolute -right-1 mt-3 w-[355px] rounded-lg border border-dark-100 bg-dark-500'>
+    <div className='absolute -right-1 z-20 mt-3 w-[355px] rounded-lg border border-dark-100 bg-dark-500'>
       <div className='absolute -top-2 right-3.5 h-4 w-4 rotate-45 border-l border-t border-dark-100 bg-dark-500'></div>
-      <h3 className='mx-4 border-b border-b-dark-100 py-3 text-base font-semibold text-light-100'>
-        Уведомления
-      </h3>
-      <div className='custom-scrollbar mb-4 max-h-96 overflow-y-auto scrollbar-thin'>
+      <h2 className='mx-4 border-b border-b-dark-100 py-3 text-base font-semibold text-light-100'>
+        {notifications.length === 0 ? 'Нет новых уведомлений' : 'Уведомления'}
+      </h2>
+      <ul className='custom-scrollbar mb-4 max-h-96 overflow-y-auto scrollbar-thin'>
         {notifications.map((notification, index) => (
-          <div className='mx-4 border-b border-gray-700 py-3' key={index}>
+          <li className='mx-4 border-b border-gray-700 py-3' key={index}>
             <div className='flex items-center'>
               <span className='text-sm font-semibold text-light-100'>Новое уведомление!</span>
               {notification.isNew && <span className='ml-1 text-xs text-accent-500'>Новое</span>}
             </div>
             <p className='text-sm text-light-100'>{notification.title}</p>
-            <p className='text-xs text-light-900'>{notification.time}</p>
-          </div>
+            <span className='text-xs text-light-900'>{notification.time}</span>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   )
 }
