abea31dcbe7a42b26e1e3cb31cd432c6c32e7f0f
KateTernovskaya <132042497+KateTernovskaya@users.noreply.github.com>
2024-11-15 13:31:02 +0500

    Bd 73 fix: header styles (#46)
    * BD-73 feat: notification bell component
    
    * BD-73 feat: notification modal
    
    * BD-73 feat: handle click out modal
    
    * BD-73 feat: notifications styles
    
    * BD-73 feat: add i18n
    
    * BD-73 fix: add hover to count note
    
    * BD-73 fix: change quotes
    
    * BD-73 fix: use effect logic in notification bell
    
    * BD-73 fix: fix use effect logic in notification bell
    
    ---------
    
    Co-authored-by: Kate <kate-ternovskaya99@mail.ru>

--- components/header-menu/MainHeaderMenu.tsx
+++ components/header-menu/MainHeaderMenu.tsx
@@ -1,49 +1,10 @@
-import { useState } from 'react'
-
-import { ConfirmationModal } from '@/components/modals'
-import { authActions } from '@/features'
-import { useTranslation } from '@/hooks/useTranslation'
-import { authApi } from '@/services'
-import { useAppDispatch } from '@/store'
-import { Button, Typography } from 'uikit-inctagram'
+import { LangSelect, NotificationBell } from '@/components'
 
 export const MainHeaderMenu = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const dispatch = useAppDispatch()
-  const { data } = authApi.useAuthMeQuery()
-  const [logout, { isLoading: isLogoutLoading }] = authApi.useLogoutMutation()
-  const { t } = useTranslation()
-
-  const handleOpenModal = () => setIsModalOpen(true)
-  const handleCloseModal = () => setIsModalOpen(false)
-  const handleLogout = () => {
-    logout()
-      .unwrap()
-      .then(() => {
-        dispatch(authActions.logout())
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
   return (
-    <div className='flex items-center gap-6'>
-      <ConfirmationModal
-        body={
-          <Typography.TextBase className='text-light-100'>
-            {t.header.logoutConfirmation(data?.email)}
-          </Typography.TextBase>
-        }
-        isLoading={isLogoutLoading}
-        isOpened={isModalOpen}
-        onClose={handleCloseModal}
-        onConfirm={handleLogout}
-        title='Confirm logout'
-      />
-      <Button onClick={handleOpenModal} variant='text'>
-        {t.header.logout}
-      </Button>
+    <div className='flex items-center gap-6 text-light-100'>
+      <NotificationBell />
+      <LangSelect />
     </div>
   )
 }
